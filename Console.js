dabe5a360bf71e34532338a33b0565aa883ff3bb
Nicolas <nikkidbz@gmail.com>
2013-06-03 16:30:11 +0200

    Possibilité de lancer que l'interface 	fuck la 3D et la 2D \o/

--- Console.js
+++ Console.js
@@ -5,7 +5,7 @@
 
 
 // Affiche les infos ?
-static var displayInfo : boolean = true ;
+static var displayInfo : boolean = false ;
 
 // Affiche les warning ?
 static var displayWarning : boolean = true ;
@@ -18,7 +18,7 @@ static var displayCriticalError : boolean = true ;
 static var displayPrivateDebug : boolean = true ;
 
 // lesquels ?
-static var PrivateDebugToDisplay : int = 48 ;
+static var PrivateDebugToDisplay : int = 50 ;
 
 
 
