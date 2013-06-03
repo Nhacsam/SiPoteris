7b4b4629a8b1398ae02a0df9a5b89992f7ad6709
Fabien <fabien.daoulas@gmail.com>
2013-06-03 16:28:41 +0100

    ajout de commentaires dans mes fichiers-nettoyage

--- getXML.js
+++ getXML.js
@@ -2,7 +2,7 @@
 	Creation : 19/04/2013
 	Author : Fabien Daoulas
 	Last update : 08/05/2013
-This script parses xml files, informations about videos.
+This script parses xml files, informations about videos...
 	
 */
 #pragma strict
@@ -18,6 +18,7 @@ private var mFile : XmlDocument ;
 	
 function InitXml( XMLToLoad : String ) {
 	
+	// because .xml is not managed by iPad, this file has to be .txt and then load as a textAsset
 	var textAsset : TextAsset = Resources.Load( XMLToLoad , typeof(TextAsset) ) as TextAsset;
 	mFile = new XmlDocument();
 	mFile.Load( new StringReader( textAsset.text ) );
@@ -37,27 +38,31 @@ function getElementFromXML( functions : Hashtable ) : Array {
 	// root hashtable
 	var rootTab : Hashtable ;
 
+	// get all element under tag "root"
 	var elementList : XmlNodeList = mFile.GetElementsByTagName( "root" );
 	
 	var rootChildren : XmlNodeList;
 	
-	
+	// get child of roots
 	for each( var rootnodeList : XmlNode in elementList ) {
 		 rootChildren =  rootnodeList.ChildNodes ;
 	 }
 	  
 	 
-	// parse XML
+	// parse XML - fir each element of rootChildren list
 	for each( var nodeList : XmlNode in rootChildren ){
-		 
 		 if(  nodeList.Name == '#comment' )
 		 	continue ;
 		 
 		rootTab = new Hashtable();
+		
+		// list of child of an element of rootChidren list
 		var elementContent : XmlNodeList = nodeList.ChildNodes;
 		
+		// explore all the xml recursively until there is no child for a node
 		exploreRecursively( elementContent , rootTab );
 		
+		// get all the information stored under this element of rootChildren
 		Data.Push( rootTab );
 		
 		if( functions.Contains(nodeList.Name) )
@@ -92,7 +97,7 @@ private function exploreRecursively ( list : XmlNodeList , Htab : Hashtable){
 		else {// if there are no childNodes
 		
 			if( Htab.ContainsKey( nodeList.Name ) && typeof( Htab[ nodeList.Name ] ) != typeof( Array ) ){ // if key already exists and this is not an array
-			
+				// create an array and start storing all the information
 				var array : Array = new Array();
 				array.Push( Htab[ nodeList.Name ] );
 				array.Push( nodeList.InnerText );
@@ -100,7 +105,7 @@ private function exploreRecursively ( list : XmlNodeList , Htab : Hashtable){
 				
 			} else if( Htab.ContainsKey( nodeList.Name ) && typeof( Htab[ nodeList.Name ] ) == typeof( Array ) ){ // if key already exists and this is an array
 			
-				// add another element
+				// add another element in the array
 				( Htab[ nodeList.Name ] as Array).Push( nodeList.InnerText );
 			
 			} else // if key does not exist already
