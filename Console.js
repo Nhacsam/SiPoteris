23164016029d521fd58b4aff7ae4501aee0dc1af
Nicolas <nikkidbz@gmail.com>
2013-06-11 16:58:46 +0200

    Changement de langue

--- Console.js
+++ Console.js
@@ -18,7 +18,7 @@ static var displayCriticalError : boolean = true ;
 static var displayPrivateDebug : boolean = true ;
 
 // lesquels ?
-static var PrivateDebugToDisplay : int = 100 ;
+static var PrivateDebugToDisplay : int = 102 ;
 
 
 
